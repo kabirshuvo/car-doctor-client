17d1edc3e5746e0dc1e7bf94b1a0915b44591a4f
kabirshuvo <kabirshuvo19@gmail.com>
2023-05-13 17:56:51 +0600

    Login and privateRoute redirrection updated

--- src/pages/LogIn/LogIn.jsx
+++ src/pages/LogIn/LogIn.jsx
@@ -1,10 +1,14 @@
 import { useContext } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../providers/AuthProvider";
 
 const LogIn = () => {
 
     const {signIn} = useContext(AuthContext);
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    const from = location.state?.from?.pathname || '/';
 
   const handleLogin = (event) => {
     event.preventDefault();
@@ -17,6 +21,7 @@ const LogIn = () => {
     .then(result => {
       const user =result.user;
       console.log(user);
+      navigate(from, {replace: true})
     })
     .catch(error => console.error(error))
 
